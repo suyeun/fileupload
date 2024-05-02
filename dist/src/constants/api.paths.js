3c0d800b8fcfb190b5cfafc955b98c0120912a0e
kimsuyeon <suyeon3715@bitbucket.org>
2024-05-02 20:39:42 +0900

    Apply .gitigmore

--- dist/src/constants/api.paths.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API = void 0;
-class API {
-}
-API.NFT_IMAGE_GALLERY = 'https://polygon-mumbai.g.alchemy.com/nft/v2/LHa8IuNu6lXI6de12LL1Uw7j6HSLCyFl/getNFTsForCollection?contractAddress=0x0552B23f5aC8561548987368B7b3B1e9124De892&withMetadata=true&startToken=1';
-exports.API = API;
-//# sourceMappingURL=api.paths.js.map
--- dist/src/domains/shared/guards/sample.guard.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=sample.guard.js.map
